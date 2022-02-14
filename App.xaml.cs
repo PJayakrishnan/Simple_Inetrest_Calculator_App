fb5a428b5d9191ebec5542c520a0035e328cbabb
Jayakrishnan P <96824788+PJayakrishnan@users.noreply.github.com>
2022-02-14 21:01:22 +0530

    commit 2

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly:ExportFont("Comfortaa-VariableFont_wght.ttf", Alias = "Comfortaa")]
+[assembly:ExportFont("CutiveMono-Regular.ttf",Alias = "cutive_mono")]
+namespace TestApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void HandleOnClick(object sender, EventArgs args)
+        {
+            double simpleinterest;
+            double amount;
+            double principle;
+            double interest;
+            double years;
+            principle = Convert.ToDouble(principleAmount.Text);
+            interest = Convert.ToDouble(interestRate.Text);
+            years = Convert.ToDouble(noOfYears.Text);
+            simpleinterest = (principle * years * interest) / 100;
+            amount = simpleinterest + principle;
+            interestLabel.Text = "Rs. " + simpleinterest;
+            amountLabel.Text = "Rs. " + amount;
+        }
+        public void HandleClearOnClick(object sender,EventArgs args)
+        {
+            principleAmount.Text = String.Empty;
+            interestRate.Text = String.Empty;
+            noOfYears.Text = String.Empty;
+            interestLabel.Text = String.Empty;
+            amountLabel.Text = String.Empty;
+        }
+    }
+}
